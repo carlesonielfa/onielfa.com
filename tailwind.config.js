8e2345ad9ca70148cc235b8a320325de29449587
carlesonielfa <carlesonielfa@gmail.com>
2023-09-07 15:53:36 +0200

    Added more animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+      transitionProperty: {
+        'height': 'height',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      animation: {
+        'blink': 'blink .75s step-end infinite',
+        'slide-out-top': 'slide_out_top 1s cubic-bezier(0.550, 0.085, 0.680, 0.530) both',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+        slide_out_top: {
+          '0%': { transform: 'translateY(0)'},
+          '100%': { transform: 'translateY(0)'},
+        },
+      }
     },
   },
   plugins: [
